0d230c55360eaedddbfb9d456ecac709c11807c7
sallanez <saul.alejandro19@gmail.com>
2022-04-19 11:37:47 -0600

    RegisterActivity added

--- app/src/main/java/sv/edu/udb/pizzeria_forkify/OrderPizza/ui/login/LoginFragment.java
+++ app/src/main/java/sv/edu/udb/pizzeria_forkify/OrderPizza/ui/login/LoginFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -23,6 +24,9 @@ public class LoginFragment extends Fragment {
 
         binding = FragmentLoginBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        final Button ejemplo = binding.register;
+        loginViewModel.goRegister(ejemplo,getContext());
 
 //        final TextView textView = binding.textSlideshow;
 //        loginViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
