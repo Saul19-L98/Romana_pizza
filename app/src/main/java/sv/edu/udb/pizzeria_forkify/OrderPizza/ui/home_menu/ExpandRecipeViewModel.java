ccddb645c4496a3ab3d9fb8a978322a6652765fd
WILLIAMS AMAYA <whistemalo@gmail.com>
2022-05-11 14:45:38 -0600

    Eliminacion de la rama Main
    se elimino debido al cambio de pizeria -> recetario

--- app/src/main/java/sv/edu/udb/pizzeria_forkify/OrderPizza/ui/home_menu/ExpandRecipeViewModel.java
+++
@@ -1,7 +0,0 @@
-package sv.edu.udb.pizzeria_forkify.OrderPizza.ui.home_menu;
-
-import androidx.lifecycle.ViewModel;
-
-public class ExpandRecipeViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
