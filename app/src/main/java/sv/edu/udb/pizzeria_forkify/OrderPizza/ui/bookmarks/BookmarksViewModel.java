e94a8c905d96d9669bdb1cee49be539103f2f416
WILLIAMS AMAYA <whistemalo@gmail.com>
2022-05-13 23:01:39 -0600

    filtrado de la lista "Mis recetas"

--- app/src/main/java/sv/edu/udb/pizzeria_forkify/OrderPizza/ui/bookmarks/BookmarksViewModel.java
+++ app/src/main/java/sv/edu/udb/pizzeria_forkify/OrderPizza/ui/bookmarks/BookmarksViewModel.java
@@ -2,6 +2,7 @@ package sv.edu.udb.pizzeria_forkify.OrderPizza.ui.bookmarks;
 
 import static sv.edu.udb.pizzeria_forkify.OrderPizza.LandingMenuActivity.database;
 import static sv.edu.udb.pizzeria_forkify.OrderPizza.LandingMenuActivity.myAuth;
+import static sv.edu.udb.pizzeria_forkify.OrderPizza.LandingMenuActivity.refUser;
 
 import android.content.Context;
 import android.util.Log;
@@ -13,6 +14,7 @@ import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -20,6 +22,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import sv.edu.udb.pizzeria_forkify.OrderPizza.model.ModelRecetas;
 import sv.edu.udb.pizzeria_forkify.OrderPizza.ui.home_menu.ModelRecetasAdapter;
@@ -35,26 +38,30 @@ public class BookmarksViewModel extends ViewModel {
     private ArrayList<String> pasosList;
     ModelRecetasAdapter modelRecetasAdapter;
 
+    ArrayList<String>  keyList = new ArrayList<>();
+
     DatabaseReference reference = FirebaseDatabase.getInstance().getReference()
             .child("RecetarioForkify")
             .child("Categoria")
             .child("Mixto");
+
 
     public BookmarksViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is slideshow fragment");
     }
 
+
+
     public LiveData<String> getText() {
         return mText;
     }
 
-
     public void getMyBookmarks(RecyclerView recyclerView, Context context) {
         //Metodo de obtencion del Nombre usuario a travez del nickname del correo
-        String userEmail=myAuth.getCurrentUser().getEmail();
-        int index = userEmail.indexOf('@');
-        userEmail = userEmail.substring(0,index);
+        String userKey=myAuth.getCurrentUser().getEmail();
+        int index = userKey.indexOf('@');
+        userKey = userKey.substring(0,index);
 //        return userEmail;
 
         listOriginal = new ArrayList<>();
@@ -65,57 +72,78 @@ public class BookmarksViewModel extends ViewModel {
         modelRecetasAdapter = new ModelRecetasAdapter(context,list);
         recyclerView.setAdapter(modelRecetasAdapter);
 
-        reference.addValueEventListener(new ValueEventListener() {
+        refUser.child(userKey).addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                list.clear();
-                listOriginal.clear();
-
-                for (DataSnapshot dataSnapshot : snapshot.getChildren()){
-                    pasosList.clear();
-                    ingredientesList.clear();
-                    Log.e("Datasnapshot", "Adapter pizza list: "+ dataSnapshot);
-
-                    for (DataSnapshot dataSnapshot1:snapshot.child(dataSnapshot.getKey()).child("Ingredientes").getChildren()){
-                        //Log.e("Ingredientes", "Adapter pizza list: "+ dataSnapshot1);
-                        ingredientesList.add(dataSnapshot1.getValue().toString());
-                    }
-
-                    for (DataSnapshot dataSnapshot2:snapshot.child(dataSnapshot.getKey()).child("Pasos").getChildren()){
-                        //Log.e("Pasos", "Adapter pizza list: "+ dataSnapshot2);
-                        pasosList.add(dataSnapshot2.getValue().toString());
-                    }
-
-                    Log.e("Listas", "Adapter pizza list: "+ pasosList );
-
-                    ModelRecetas modelRecetasItem =new ModelRecetas(
-                            snapshot.child(dataSnapshot.getKey()).child("titulo").getValue().toString(),
-                            snapshot.child(dataSnapshot.getKey()).child("refImg").getValue().toString(),
-                            snapshot.child(dataSnapshot.getKey()).child("descripcion").getValue().toString(),
-                            snapshot.child(dataSnapshot.getKey()).child("tiempo").getValue().toString(),
-                            (ArrayList<String>) ingredientesList.clone(), (ArrayList<String>) pasosList.clone(),
-                            Integer.parseInt(snapshot.child(dataSnapshot.getKey()).child("noPersonas").getValue().toString())
-                    );
-                    modelRecetasItem.setKey(dataSnapshot.getKey());
-
-
-                    list.add(modelRecetasItem);
-
+                for (DataSnapshot dataSnapshot: snapshot.getChildren()){
+                    keyList.add(dataSnapshot.getValue().toString());
                 }
 
-                listOriginal.addAll(list);
-                modelRecetasAdapter.notifyDataSetChanged();
+                reference.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        list.clear();
+                        listOriginal.clear();
+
+                        for (DataSnapshot dataSnapshot : snapshot.getChildren()){
+                            pasosList.clear();
+                            ingredientesList.clear();
+                            Log.e("Datasnapshot", "Adapter pizza list: "+ dataSnapshot);
+
+                            for (DataSnapshot dataSnapshot1:snapshot.child(dataSnapshot.getKey()).child("Ingredientes").getChildren()){
+                                //Log.e("Ingredientes", "Adapter pizza list: "+ dataSnapshot1);
+                                ingredientesList.add(dataSnapshot1.getValue().toString());
+                            }
+
+                            for (DataSnapshot dataSnapshot2:snapshot.child(dataSnapshot.getKey()).child("Pasos").getChildren()){
+                                //Log.e("Pasos", "Adapter pizza list: "+ dataSnapshot2);
+                                pasosList.add(dataSnapshot2.getValue().toString());
+                            }
+
+                            Log.e("Listas", "Adapter pizza list: "+ pasosList );
+
+                            ModelRecetas modelRecetasItem =new ModelRecetas(
+                                    snapshot.child(dataSnapshot.getKey()).child("titulo").getValue().toString(),
+                                    snapshot.child(dataSnapshot.getKey()).child("refImg").getValue().toString(),
+                                    snapshot.child(dataSnapshot.getKey()).child("descripcion").getValue().toString(),
+                                    snapshot.child(dataSnapshot.getKey()).child("tiempo").getValue().toString(),
+                                    (ArrayList<String>) ingredientesList.clone(), (ArrayList<String>) pasosList.clone(),
+                                    Integer.parseInt(snapshot.child(dataSnapshot.getKey()).child("noPersonas").getValue().toString())
+                            );
+                            modelRecetasItem.setKey(dataSnapshot.getKey());
+
+                            if (keyList.contains(dataSnapshot.getKey())){
+                                list.add(modelRecetasItem);
+                            }
+
+                        }
+
+                        listOriginal.addAll(list);
+                        modelRecetasAdapter.notifyDataSetChanged();
+
+                    }
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+
+                });
 
             }
+
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
 
             }
-
         });
+
+
+
+
 
         return;
     }
+
 
     public void searchTXT(SearchView searchView, Context context) {
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
@@ -131,6 +159,5 @@ public class BookmarksViewModel extends ViewModel {
             }
         });
     }
-
 
 }
